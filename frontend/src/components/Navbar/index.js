7f1299877b152f67ebeefd9f86ce6c7229dce75d
Ana Hintz <anahintz32@gmail.com>
2024-03-21 17:23:02 -0300

    background

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,19 +1,18 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
-import HomeIcon from '@mui/icons-material/Home';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import PeopleIcon from '@mui/icons-material/People';
-
+import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import PeopleIcon from "@mui/icons-material/People";
 
 export default function AnchorTemporaryDrawer() {
   const [state, setState] = React.useState({
@@ -24,7 +23,10 @@ export default function AnchorTemporaryDrawer() {
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
@@ -33,59 +35,63 @@ export default function AnchorTemporaryDrawer() {
 
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250  }}
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250}}
       role="presentation"
-     
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List sx={{ color:"#f34f8b"}}>
-      
-        {['Home', 'Doar'].map((text, index) => (
+      <List sx={{ color: "#f34f8b", background: "#625b5b" }}>
+        {["Home", "Doar"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <HomeIcon  sx={{ color:"#f34f8b" }} /> : <FavoriteIcon  sx={{ color:"#f34f8b" }} />  }
-    
+                {index % 2 === 0 ? (
+                  <HomeIcon sx={{ color: "#f34f8b" }} />
+                ) : (
+                  <FavoriteIcon sx={{ color: "#f34f8b" }} />
+                )}
               </ListItemIcon>
-             
+
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
-      <List sx={{ color:"#f34f8b" }}>
-        {['Quem somos', 'Contato'].map((text, index) => (
+      <List sx={{ color: "#f34f8b", background: "#625b5b" }}>
+        {["Quem somos", "Contato"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <PeopleIcon  sx={{ color:"#f34f8b" }} /> : <LocalPhoneIcon  sx={{ color:"#f34f8b" }} />  }
-    
+                {index % 2 === 0 ? (
+                  <PeopleIcon sx={{ color: "#f34f8b" }} />
+                ) : (
+                  <LocalPhoneIcon sx={{ color: "#f34f8b" }} />
+                )}
               </ListItemIcon>
-             
+
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
-      <Divider />
-     
+      <Divider  />
     </Box>
   );
 
   return (
     <div>
-      
-      {['Menu'].map((anchor) => (
+      {["Menu"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}
-          sx={{ color:"#f34f8b" }}
-          >{anchor}</Button>
+          <Button
+            onClick={toggleDrawer(anchor, true)}
+            sx={{ color: "#f34f8b" }}
+          >
+            {anchor}
+          </Button>
           <Drawer
             anchor={anchor}
             open={state[anchor]}
             onClose={toggleDrawer(anchor, false)}
-            
           >
             {list(anchor)}
           </Drawer>
